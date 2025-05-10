269797a9c4ff41ac2752a99652220615a797a166
JamesK-XIV <jameskidman14@gmail.com>
2025-05-10 16:11:24 +1000

    Final Anim changes

--- Assets/BarrelQTE/GroundAndBarrel.cs
+++ Assets/BarrelQTE/GroundAndBarrel.cs
@@ -1,15 +1,35 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GroundAndBarrel : MonoBehaviour
 {
     public GameObject ground;
     public GameObject barrel;
     private float timer;
+    public Image jump;
     public bool spawn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+    }
+
+    public void QTE()
+    {
+        StartCoroutine(SpaceToJump());
+    }
+
+    IEnumerator SpaceToJump()
+    {
+        if (jump && jump.IsActive() == false)
+        {
+
+            yield return new WaitForSeconds(0.2f);
+            jump.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.8f);
+            jump.gameObject.SetActive(false);
+        }
     }
 
     public void SpawnPipe()
@@ -25,8 +45,12 @@ public class GroundAndBarrel : MonoBehaviour
             if (timer > 2)
             {
                 //Instantiate(ground).transform.position = gameObject.transform.position;
-                Instantiate(barrel).transform.position = gameObject.transform.position + new Vector3(0, 0.25f, 0);
-                Instantiate(barrel).transform.position = gameObject.transform.position + new Vector3(-1.5f, 0.25f, 0);
+                GameObject b1 = Instantiate(barrel);
+                b1.transform.position = gameObject.transform.position + new Vector3(0, 0.25f, 0);
+                b1.GetComponent<MoveForwards>().jump = jump;
+                GameObject b2 = Instantiate(barrel);
+                b2.transform.position = gameObject.transform.position + new Vector3(-1.3f, 0.25f, 0);
+                b2.GetComponent<MoveForwards>().jump = jump;
                 timer = 0;
             }
         }
--- Assets/BarrelQTE/MoveForwards.cs
+++ Assets/BarrelQTE/MoveForwards.cs
@@ -1,13 +1,19 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveForwards : MonoBehaviour
 {
     public int speed;
     public bool teleport;
+    public Image jump;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!teleport)
+        {
+            FindAnyObjectByType<GroundAndBarrel>().QTE();
+        }
     }
 
     // Update is called once per frame
