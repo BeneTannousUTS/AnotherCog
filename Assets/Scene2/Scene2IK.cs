308951bf1b7cdd99947327e3b42de95a115be33f
Benedict Tannous <benedict.m.tanous@student.uts.edu.au>
2025-05-12 07:24:48 +1000

    Further progressed sequencing and added more props to scene

---
+++ Assets/Scene2/Scene2IK.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Scene2IK : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] GameObject target;
+    
+    float weight = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnTracking()
+    {
+        weight = 1;
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetLookAtPosition(target.transform.position);
+        anim.SetLookAtWeight(weight);
+    }
+}
