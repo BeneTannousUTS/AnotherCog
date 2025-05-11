8c1518511c5a5dc2593f75483476862fa78d8098
Mesmaa <60990360+Julianm-25@users.noreply.github.com>
2025-05-11 19:20:14 +1000

    finished

--- Assets/ThirdPart/ThirdPartHelper.cs
+++ Assets/ThirdPart/ThirdPartHelper.cs
@@ -6,9 +6,15 @@ public class ThirdPartHelper : MonoBehaviour
 
     public Animator workerAnim, cogAnim, bossAnim;
     public GameObject bossHead;
-    private float t = 0.0f, bossT = 0.0f;
+    private float t = 0.0f, bossT = 0.0f, fadeT = 0.0f;
     public Avatar fallAvatar;
-    private bool isLookingAtBoss;
+    private bool isLookingAtBoss, isFadeOut;
+    public GameObject fireworks, cat1, cat2;
+    public AudioSource catsource;
+    public AudioClip catclip;
+    public GameObject mask;
+    private float maskSize = 2000f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,15 +30,50 @@ public class ThirdPartHelper : MonoBehaviour
             workerAnim.transform.GetComponent<IKLookAt>().weight = Mathf.Lerp(0,1, bossT/4);
             bossT += Time.deltaTime;
         }
+        if (isFadeOut) {
+            mask.GetComponent<RectTransform>().sizeDelta = new Vector2(maskSize, maskSize);
+            maskSize = Mathf.Lerp(2000,0, fadeT/3);
+            fadeT += Time.deltaTime;
+        }
     }
 
     public IEnumerator AnimationHelp() {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(4);
+        StartCoroutine("Fireworks");
+        yield return new WaitForSeconds(4);
+        cat1.GetComponent<ParticleSystem>().Play();
+        cat2.GetComponent<ParticleSystem>().Play();
+        StartCoroutine("CatSounds");
+        yield return new WaitForSeconds(3);
+        cat1.GetComponent<ParticleSystem>().Stop();
+        cat2.GetComponent<ParticleSystem>().Stop();
         workerAnim.SetTrigger("tired");
-        yield return new WaitForSeconds(6);
+        yield return new WaitForSeconds(5);
         workerAnim.transform.GetComponent<IKLookAt>().weight = 0;
         workerAnim.transform.GetComponent<IKLookAt>().target = bossHead;
         isLookingAtBoss = true;
         workerAnim.SetTrigger("fall");
+        yield return new WaitForSeconds(8);
+        isFadeOut = true;
+    }
+
+    public IEnumerator Fireworks() {
+        foreach (Transform firework in fireworks.transform) {
+            firework.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+
+    public IEnumerator CatSounds() {
+        catsource.PlayOneShot(catclip);
+        yield return new WaitForSeconds(0.5f);
+        catsource.PlayOneShot(catclip);
+        yield return new WaitForSeconds(0.5f);
+        catsource.PlayOneShot(catclip);
+        yield return new WaitForSeconds(0.5f);
+        catsource.PlayOneShot(catclip);
+        yield return new WaitForSeconds(0.5f);
+        catsource.PlayOneShot(catclip);
+        yield return new WaitForSeconds(0.5f);
     }
 }
